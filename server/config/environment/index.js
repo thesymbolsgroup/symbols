5a5fb0a3fa9d840f0405ff4b86d05a684a256490
thesymbolsgroup <thesymbolsgroup@gmail.com>
2016-07-13 18:34:37 +0100

    Addition of oauth2callback

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -50,7 +50,7 @@ var all = {
   google: {
     clientID:     process.env.GOOGLE_ID || 'id',
     clientSecret: process.env.GOOGLE_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback'
+    callbackURL:  (process.env.DOMAIN || '') + '/oauth2callback'
   }
 };
 
--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -9,11 +9,11 @@ module.exports = {
   DOMAIN:           'http://localhost:9000',
   SESSION_SECRET:   'test3-secret',
 
-  FACEBOOK_ID:      'app-id',
-  FACEBOOK_SECRET:  'secret',
+  FACEBOOK_ID:      '2054447374780894',
+  FACEBOOK_SECRET:  'b29504bb1613f3c8ae2baac1694cbc50',
 
-  GOOGLE_ID:        'app-id',
-  GOOGLE_SECRET:    'secret',
+  GOOGLE_ID: '415326202032-uivtii7aedd32m4pihq72ai1for25u36.apps.googleusercontent.com',
+  GOOGLE_SECRET:    'ct0Xz-oYEksVD9qgG5OBFsbF',
 
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
