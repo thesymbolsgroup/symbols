3072d02d7eb0a8e56385bad8b1b3f8432165ae7b
thesymbolsgroup <charliemarjot@Charlies-MacBook-Pro.local>
2016-07-21 15:42:11 +0100

    Additional Facebook fields
    Added gender to Facebook API request

--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -8,26 +8,32 @@ export function setup(User, config) {
     callbackURL: config.facebook.callbackURL,
     profileFields: [
       'displayName',
-      'emails'
+      'emails',
+      'gender'
     ]
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({'facebook.id': profile.id}).exec()
       .then(user => {
         if (user) {
+          console.log(user);
           return done(null, user);
+            
         }
 
         user = new User({
           name: profile.displayName,
           email: profile.emails[0].value,
           role: 'user',
+          gender: profile.gender,
           provider: 'facebook',
           facebook: profile._json
         });
         user.save()
           .then(user => done(null, user))
           .catch(err => done(err));
+        
+        console.log(user);
       })
       .catch(err => done(err));
   }));
